744763dddb51e379cf4b7fefc01fd6bf3f787a7b
max141838 <fire4max@gmail.com>
2023-07-25 19:39:20 +0300

    number-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import {Link,Routes,Route} from "react-router-dom";
+import Home from "./page/Home";
+import Layout from "./page/Layout";
+import About from "./page/About";
 const App = () => {
     return (
         <div>
-        <h1>Hello</h1>
+            <div>
+                <h2>Text</h2>
+                <ul>
+                    <li><Link to={"/"}>home</Link></li>
+                    <li><Link to={"/layout"}>layout</Link></li>
+                    <li><Link to={"/about"}>about</Link></li>
+                </ul>
+            </div>
+            <div>
+                <h2>content</h2>
+                <Routes>
+                    <Route path={"/"} element={Home}/>
+                    <Route path={"/layout"} element={Layout}/>
+                    <Route path={"/about"} element={About}/>
+                </Routes>
+            </div>
         </div>
     );
 };
---
+++ src/page/Layout.js
@@ -0,0 +1,13 @@
+import {Outlet} from "react-router-dom";
+
+export default function Layout(){
+    return(
+        <div>
+            Layout
+            <Outlet/>
+        </div>
+    );
+}
+
+
+
