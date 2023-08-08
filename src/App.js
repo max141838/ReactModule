5f12c30cf9da56449f303d17a245d3cbaf0efccc
max141838 <fire4max@gmail.com>
2023-08-08 13:27:23 +0300

    number-4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css'
 import Remake from "./tools/toolsPerson";
+import UserCom from "./tools/userCom";
 const App = () => {
+    let [users,setUsers] = useState([]);
+    useEffect( () => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                setUsers(value)
+                console.log(value)
+            });
+    });
+
+
     return (
         <div className={'wrap'}>
         <h1>Hello</h1>
@@ -11,6 +23,12 @@ const App = () => {
             <Remake
             itemName={'Kyle Broflovski'}
             pic={'https://static.wikia.nocookie.net/southpark/images/4/4c/HighJewElfKing.transparent.png/revision/latest?cb=20190411032218'}/>
+
+            {
+                users.map(value => <UserCom name={value.name} email={value.email} />)
+            }
+
+
         </div>
     );
 };
---
+++ src/tools/userCom.js
@@ -0,0 +1,16 @@
+ export default function UserCom (mops) {
+    let [name,email] = mops
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+}
+
+
+
+
+
+
+
